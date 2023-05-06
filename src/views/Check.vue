bd5aac4f699732b25051ff4c1923071a0d5c0cfa
laoningmeng <liu529260424@163.com>
2023-05-06 17:46:39 +0800

    add layout

---
+++ src/views/Check.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <a-list item-layout="horizontal" :data-source="data" style="text-align: left;">
+      <template #renderItem="{ item }">
+        <a-list-item>
+          <a-space style="margin-right:20px;">
+            <check-circle-outlined style="color: #52c41a; font-size:20px;"/>
+            <exclamation-circle-outlined style="color: gray; font-size:20px;"/>
+            <close-circle-outlined style="color: #eb2f96; font-size:20px;" />
+          </a-space>
+          <a-list-item-meta
+              description="ready to check"
+          >
+            <template #title>
+              <span >{{ item.title }}</span>
+            </template>
+          </a-list-item-meta>
+        </a-list-item>
+      </template>
+    </a-list>
+    <div>
+      <a-row  justify="center" style="margin-top:20px">
+        <a-button type="primary" >开始检测</a-button>
+      </a-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Check"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/layout/Index.vue
@@ -0,0 +1,72 @@
+<template>
+  <a-layout class="layout">
+    <a-layout-header>
+      <span class="logo">安装程序</span>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px; margin-top:20px">
+      <a-card  :bordered="false"  style="padding: 20px;">
+        <div :style="{ background: '#fff', minHeight: '100px'}" aclass="height-100">
+          <a-row type="flex" justify="space-around" style="margin-top:20px">
+            <a-steps :current="current_step">
+              <a-step v-for="step in steps" :title="step.title"  />
+            </a-steps>
+          </a-row>
+        </div>
+        <div>
+          <router-view :key="key" />
+        </div>
+      </a-card>
+
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<script>
+export default {
+  name: "Layout",
+  data(){
+    return {
+      steps:[
+        {
+          title:"环境检测",
+          sort: 0
+        },
+        {
+          title:"配置信息",
+          sort: 0
+        },
+        {
+          title:"waitting",
+          sort: 0
+        },
+        {
+          title:"环境检测",
+          sort: 0
+        },
+        {
+          title:"配置信息",
+          sort: 0
+        },
+        {
+          title:"waitting",
+          sort: 0
+        }
+      ],
+      current_step:0,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.logo{
+  color:white;
+}
+*{
+  margin:0px;
+  padding: 0px;
+}
+</style>
